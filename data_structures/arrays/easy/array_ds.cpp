b7abcd06d59e8f6ca76f8e902bdbd7260f987464
Subbu <subnr01@gmail.com>
2017-03-31 22:13:40 -0400

    Create array_ds.cpp

---
+++ data_structures/arrays/easy/array_ds.cpp
@@ -0,0 +1,25 @@
+
+
+https://www.hackerrank.com/challenges/arrays-ds
+
+
+/*
+
+Print an array in reverse manner
+
+*/
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    
+    for (int i = n-1; i >=0; i--) {
+        cout<<arr[i]<< " ";
+    }
+    return 0;
+}
+
